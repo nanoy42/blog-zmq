640928dc800711ff18edd1824e3ef672792aa1c1
Yoann Pietri <me@nanoy.fr>
2020-04-06 22:57:26 +0200

    Update license

--- schemas_base/puller.py
+++ schemas_base/puller.py
@@ -1,6 +1,23 @@
-import zmq
-import time
+# Blog ZMQ source code
+# Copyright (C) 2020 Yoann Pietri
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import random
+import time
+
+import zmq
 
 context = zmq.Context()
 puller = context.socket(zmq.PULL)
@@ -10,5 +27,5 @@ puller.connect("tcp://localhost:5555")
 
 while True:
     print(puller.recv_string())
-    time.sleep(random.randint(1,5)) # execute la tache
+    time.sleep(random.randint(1, 5))  # execute la tache
     print("Task done")
